8e3b002f1477fd21980a34aad7572c7ffe9f1eca
gabrielluislopes <ogabrielluislopes@gmail.com>
2024-01-03 16:53:07 -0300

    Adding api gateway, rabbitmq and mvn packaging

--- notification/src/main/java/com/gabriellopes/notification/NotificationApplication.java
+++ notification/src/main/java/com/gabriellopes/notification/NotificationApplication.java
@@ -3,9 +3,30 @@ package com.gabriellopes.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.gabriellopes.notification",
+                "com.gabriellopes.amqp",
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//            ) {
+//        return args -> {
+//            producer.publish(
+//                    new Person("Ali", 18),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
+
 }
